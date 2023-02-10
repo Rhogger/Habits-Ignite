e152c572606dc00fdb374c59bc4a07b7f2dc23eb
rhogger <rhoggerrv@gmail.com>
2023-02-10 15:36:37 -0300

    :construction: Progresso de 80min/99min de aula-4

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import { TouchableOpacity, Dimensions, TouchableOpacityProps } from 'react-native'
+import { generateProgressPercentage } from '../utils/generate-progress-percentage'
 
 // Dias da semana
 const weekDays = 7
@@ -9,9 +10,16 @@ export const dayMarginBetween = 8
 // Tamanho (responsivo) dos elementos (quadradinhos)
 export const daySize = Dimensions.get('screen').width / weekDays - (screenHorizontalPadding + 5)
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+	amountOfHabits?: number
+	amountCompleted?: number
+	date: Date
+}
 
-export function HabitDay({ ...rest }: Props) {
+export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
+	const amountAccomplishedPercentage =
+		amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0
+
 	return (
 		<TouchableOpacity
 			className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,10 @@
-import { View, Text, ScrollView } from 'react-native'
+import { useState, useEffect } from 'react'
+import { View, Text, ScrollView, Alert } from 'react-native'
 
 // Components
 import { HabitDay, daySize } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { Loading } from '../components/Loading'
 
 // Config
 import colors from 'tailwindcss/colors'
@@ -13,14 +15,48 @@ import { useNavigation } from '@react-navigation/native'
 
 // Utils
 import { generateDatesFromYearsBeginning } from '../utils/generate-dates-from-years-beginning'
+import { api } from '../lib/axios'
+import dayjs from 'dayjs'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateDatesFromYearsBeginning()
 const minimumSummaryDatesSizes = 18 * 6
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length
 
+type SummaryProps = Array<{
+	id: string
+	date: string
+	amount: number
+	completed: number
+}>
+
 export function Home() {
+	const [loading, setLoading] = useState(true)
+	const [summary, setSummary] = useState<SummaryProps | null>(null)
+
 	const { navigate } = useNavigation()
+
+	async function fetchData() {
+		try {
+			setLoading(true)
+			const response = await api.get('/summary')
+			console.log(response.data)
+			setSummary(response.data)
+		} catch (error) {
+			Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos"')
+			console.log(error)
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	if (!loading) {
+		return <Loading />
+	}
 
 	return (
 		<View className="flex-1 bg-background px-8 pt-16">
@@ -41,23 +77,34 @@ export function Home() {
 				showsVerticalScrollIndicator={false}
 				contentContainerStyle={{ paddingBottom: 100 }}
 			>
-				<View className="flex-row flex-wrap">
-					{datesFromYearStart.map((date) => (
-						<HabitDay
-							key={date.toISOString()}
-							onPress={() => navigate('habit', { date: date.toISOString() })}
-						/>
-					))}
+				{summary && (
+					<View className="flex-row flex-wrap">
+						{datesFromYearStart.map((date) => {
+							const dayWithHabits = summary.find((day) => {
+								return dayjs(date).isSame(day.date, 'day')
+							})
 
-					{amountOfDaysToFill > 0 &&
-						Array.from({ length: amountOfDaysToFill }).map((_, i) => (
-							<View
-								key={i}
-								className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-								style={{ width: daySize, height: daySize }}
-							/>
-						))}
-				</View>
+							return (
+								<HabitDay
+									key={date.toISOString()}
+									date={date}
+									amountOfHabits={dayWithHabits?.amount}
+									amountCompleted={dayWithHabits?.completed}
+									onPress={() => navigate('habit', { date: date.toISOString() })}
+								/>
+							)
+						})}
+
+						{amountOfDaysToFill > 0 &&
+							Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+								<View
+									key={i}
+									className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+									style={{ width: daySize, height: daySize }}
+								/>
+							))}
+					</View>
+				)}
 			</ScrollView>
 		</View>
 	)
