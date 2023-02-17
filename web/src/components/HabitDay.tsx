1e8c93f8fb8f9520c4a851b9c84a667e7206f581
rhogger <rhoggerrv@gmail.com>
2023-02-17 19:46:32 -0300

    :construction: Progresso de 47min/96min de aula-5

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -30,17 +30,23 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
 	return (
 		<Popover.Root>
 			<Popover.Trigger
-				className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg', {
-					'bg-zinc-900 border-zinc-800': completedPercentage === 0,
-					'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
-					'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
-					'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
-					'bg-violet-600 border-violet-400': completedPercentage >= 60 && completedPercentage < 80,
-					'bg-violet-500 border-violet-300': completedPercentage >= 80,
-				})}
+				className={clsx(
+					'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-900 focus:ring-offset-2 focus:ring-offset-background',
+					{
+						'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+						'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+						'bg-violet-800 border-violet-600':
+							completedPercentage >= 20 && completedPercentage < 40,
+						'bg-violet-700 border-violet-500':
+							completedPercentage >= 40 && completedPercentage < 60,
+						'bg-violet-600 border-violet-400':
+							completedPercentage >= 60 && completedPercentage < 80,
+						'bg-violet-500 border-violet-300': completedPercentage >= 80,
+					}
+				)}
 			/>
 			<Popover.Portal>
-				<Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 border border-violet-500 flex flex-col">
+				<Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 border border-violet-500 flex flex-col focus:outline-none focus:ring-2 focus:ring-violet-900 focus:ring-offset-2 focus:ring-offset-background">
 					<span className="font-semibold text-zinc-400">{dayOfWeek}</span>
 					<span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -66,7 +66,7 @@ export function NewHabitForm() {
 				id="title"
 				placeholder="Ex.: Exercícios, dormir bem, etc..."
 				autoFocus
-				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-900 focus:ring-offset-2 focus:ring-offset-zinc-900"
 				value={title}
 				onChange={(event) => setTitle(event.target.value)}
 			/>
@@ -83,13 +83,13 @@ export function NewHabitForm() {
 					return (
 						<Checkbox.Root
 							key={weekDay}
-							className="flex items-center gap-3 group"
+							className="flex items-center gap-3 group focus:outline-none"
 							checked={weekDays.includes(index)}
 							onCheckedChange={() => {
 								handleToggleWeekDay(index)
 							}}
 						>
-							<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+							<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-green-900 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
 								<Checkbox.Indicator>
 									<Check
 										size={20}
@@ -105,7 +105,7 @@ export function NewHabitForm() {
 
 			<button
 				type="submit"
-				className="mt-6 rounded-lg gap-3 p-4 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-800"
+				className="mt-6 rounded-lg gap-3 p-4 flex items-center justify-center font-semibold bg-green-600 hover:bg-green-800 transition-colors focus:outline-none focus:ring-2 focus:ring-green-900 focus:ring-offset-2 focus:ring-offset-zinc-900"
 			>
 				<Check
 					size={20}
